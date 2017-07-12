3d0804e2197b9107ffa83faee03a5cd116ecc030
Temur Usupov <yusupov.temur@gmail.com>
2017-07-12 19:26:46 +0500

    change uri

--- src/main/java/com/usupov/productcard/config/PartRestURIConstants.java
+++ src/main/java/com/usupov/productcard/config/PartRestURIConstants.java
@@ -6,5 +6,6 @@ public class PartRestURIConstants {
      * Part
      */
     public static final String CREATE = "/car/{carId}/category/{categoryId}/create";
+    public static final String GET    = "/car/{carId}/part";
 
 }
